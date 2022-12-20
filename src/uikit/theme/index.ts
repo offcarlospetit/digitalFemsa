e5a56832904d4ae7d8f425f7cc98e371d3d4e5c0
Carlos Petit <ca.alberto.p@gmail.com>
2022-12-20 11:55:45 -0500

    wip: add text styles and colors

--- src/uikit/theme/index.ts
+++ src/uikit/theme/index.ts
@@ -1,4 +1,5 @@
 import { createTheme } from '@shopify/restyle';
+import { StyleProp } from 'react-native';
 
 const palette = {
     purpleLight: '#8C6FF7',
@@ -11,12 +12,20 @@ const palette = {
 
     black: '#0B0B0B',
     white: '#F0F2F3',
+    pureWhite: '#FFFFFF',
+
+    grayColor: '#9B9898',
+    headerBackGround: '#CFD6FF',
+
+    mainBlue: '#334FFA',
 };
 
 const theme = createTheme({
     colors: {
         mainBackground: palette.white,
         cardPrimaryBackground: palette.purplePrimary,
+        mainBlackText: palette.black,
+        ...palette
     },
     spacing: {
         s: 8,
@@ -30,6 +39,103 @@ const theme = createTheme({
     },
     textVariants: {
         defaults: {
+            fontWeight: "400",
+            fontSize: 12,
+            lineHeight: 16,
+            color: 'mainBlackText',
+        },
+        itemTitle: {
+            fontWeight: "800",
+            fontSize: 14,
+            lineHeight: 19.2,
+            color: 'mainBlackText',
+        },
+        pointAmount: {
+            fontWeight: "800",
+            fontSize: 16,
+            lineHeight: 21.86,
+            color: 'mainBlackText',
+        },
+        itemDate: {
+            fontWeight: "400",
+            fontSize: 12,
+            lineHeight: 16,
+            color: 'mainBlackText',
+        },
+        movementsResumeTitle: {
+            fontWeight: "800",
+            fontSize: 14,
+            lineHeight: 19.12,
+            color: 'grayColor',
+        },
+        mainAmount: {
+            fontWeight: "800",
+            fontSize: 32,
+            lineHeight: 43.71,
+            color: 'pureWhite',
+        },
+        monthMainAmount: {
+            fontWeight: "800",
+            fontSize: 16,
+            lineHeight: 21.86,
+            color: 'mainBlackText',
+        },
+        myPointsTitle: {
+            fontWeight: "800",
+            fontSize: 14,
+            lineHeight: 19.12,
+            color: 'grayColor',
+        },
+        welcomeTitle: {
+            fontWeight: "800",
+            fontSize: 20,
+            lineHeight: 27.32,
+            color: 'mainBlackText',
+        },
+        welcomeSubtitle: {
+            fontWeight: "400",
+            fontSize: 16,
+            lineHeight: 21.86,
+            color: 'mainBlackText',
+        },
+        detailHeaderTitle: {
+            fontWeight: "800",
+            fontSize: 24,
+            lineHeight: 24,
+        },
+        detailTitle: {
+            fontWeight: "800",
+            fontSize: 14,
+            lineHeight: 19.12,
+            color: 'grayColor',
+        },
+        detailTextDescription: {
+            fontWeight: "800",
+            fontSize: 16,
+            lineHeight: 21.86,
+        },
+        detailResumeText: {
+            fontWeight: "800",
+            fontSize: 14,
+            lineHeight: 19.12,
+            color: 'grayColor',
+        },
+        detailResumeAmount: {
+            fontWeight: "800",
+            fontSize: 24,
+            lineHeight: 32.78,
+        },
+        buttonLargeText: {
+            fontWeight: "800",
+            fontSize: 16,
+            lineHeight: 21.86,
+            color: 'pureWhite',
+        },
+        buttonMediumText: {
+            fontWeight: "800",
+            fontSize: 12,
+            lineHeight: 16.39,
+            color: 'pureWhite',
         },
     },
 });
