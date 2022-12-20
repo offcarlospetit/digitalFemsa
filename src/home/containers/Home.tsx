db04aa61cb43337b76083580978b256c5c6f8434
Carlos Petit <ca.alberto.p@gmail.com>
2022-12-20 11:08:38 -0500

    wip: create the startup component and expose it

---
+++ src/home/containers/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Box, Text} from '../../uikit';
+
+type Props = {};
+
+const Home: React.FC<Props> = ({}) => {
+  return (
+    <Box flex={1} justifyContent="center">
+      <Text>Home</Text>
+    </Box>
+  );
+};
+
+export default Home;
