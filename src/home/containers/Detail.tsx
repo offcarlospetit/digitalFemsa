7b6a96b9e4e18719e918eed5c845ed202e0a8a2c
Carlos Petit <ca.alberto.p@gmail.com>
2022-12-21 08:23:07 -0500

    wip: update home and detail screen

--- src/home/containers/Detail.tsx
+++ src/home/containers/Detail.tsx
@@ -1,16 +1,69 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {Box, Button, Card, Text, Utils} from '../../uikit';
+import {Image, ScrollView} from 'react-native';
+import {HomeStackParamList} from '../navigation/HomeStack';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import useHome from '../hooks/useHome';
 
-type Props = {};
+export type DetailScreenNavigationProp = NativeStackScreenProps<
+  HomeStackParamList,
+  'Detail'
+>;
 
-const Detail: React.FC<Props> = ({}) => {
+const Detail: React.FC<DetailScreenNavigationProp> = ({route, navigation}) => {
+  const {title, createdAt, points, image} = route.params;
+
+  const {showDate, formatPoints} = useHome();
+
+  const handleNavigateToHome = () => {
+    navigation.navigate('Home');
+  };
   return (
-    <View>
-      <Text>Detail</Text>
-    </View>
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <Card
+        justifyContent="flex-end"
+        paddingHorizontal="m"
+        paddingBottom="m"
+        variant="detailHeaderVariant">
+        <Text variant="detailHeaderTitle">{title}</Text>
+      </Card>
+      <Box paddingHorizontal="m" marginTop="m">
+        <Card
+          variant="imageCard"
+          paddingHorizontal="extraHorizontal"
+          backgroundColor="baseBackground"
+          paddingVertical="extraVertical">
+          <Image
+            source={image ? {uri: image} : Utils.imagePreview}
+            style={{width: 200, height: 200, resizeMode: 'cover'}}
+          />
+        </Card>
+      </Box>
+      <Box paddingHorizontal="m" marginTop="ll">
+        <Text variant="detailResumeText">{'Detalles del producto:'}</Text>
+      </Box>
+      <Box paddingHorizontal="m" marginTop="m">
+        <Text variant="detailTextDescription">
+          {'Comprado el ' + showDate(createdAt)}
+        </Text>
+      </Box>
+      <Box paddingHorizontal="m" marginTop="m">
+        <Text variant="detailResumeText">{'Con esta compra acumulaste:'}</Text>
+      </Box>
+      <Box paddingHorizontal="m" marginTop="ll">
+        <Text variant="detailResumeAmount">
+          {formatPoints(points) + ' puntos'}
+        </Text>
+      </Box>
+      <Box paddingHorizontal="m" marginTop="xll" marginBottom="xl">
+        <Button
+          onPress={handleNavigateToHome}
+          variant="buttonPrimary"
+          label="Aceptar"
+        />
+      </Box>
+    </ScrollView>
   );
 };
 
 export default Detail;
-
-const styles = StyleSheet.create({});
--- src/home/containers/Home.tsx
+++ src/home/containers/Home.tsx
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Box, Button, Card, Text} from '../../uikit';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import Item from '../components/Item';
 import {ScrollView} from 'react-native';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {HomeStackParamList} from '../navigation/HomeStack';
+import {useGetProductsQuery} from '../hooks';
+import useHome from '../hooks/useHome';
 
-type Props = {};
+export type HomeScreenNavigationProp = NativeStackScreenProps<
+  HomeStackParamList,
+  'Home'
+>;
 
-const Home: React.FC<Props> = ({}) => {
+const Home: React.FC<HomeScreenNavigationProp> = ({navigation}) => {
   const {top} = useSafeAreaInsets();
+  const {
+    data,
+    getAllPoints,
+    handleShowAll,
+    handleShowRedeemed,
+    handleShowWins,
+    showAll,
+    showWins,
+    showDate,
+    formatPoints,
+    getMonthFromData,
+  } = useHome();
+
+  const handleNavigateToDetail = (index: number) => {
+    if (!data) return;
+    const {product, createdAt, points, image} = data[index];
+    navigation.navigate('Detail', {
+      title: product,
+      createdAt: createdAt,
+      points: points,
+      image: image,
+    });
+  };
+
   return (
     <Box flex={1} backgroundColor="baseBackground">
       <Box paddingLeft="m" paddingTop="s" style={{marginTop: top + 13}}>
@@ -19,13 +50,13 @@ const Home: React.FC<Props> = ({}) => {
       <Box paddingLeft="m" marginBottom="m" marginTop="m">
         <Text variant="myPointsTitle">TUS PUNTOS</Text>
       </Box>
-      <Box paddingHorizontal="xxl" justifyContent="center" alignItems="center">
+      <Box paddingHorizontal="xl" justifyContent="center" alignItems="center">
         <Card paddingBottom="l" paddingTop="m" variant="elevated">
           <Text marginLeft="m" variant="monthMainAmount">
-            Diciembre
+            {getMonthFromData()}
           </Text>
           <Text marginTop="s" textAlign="center" variant="mainAmount">
-            10,00.00 pts
+            {getAllPoints()}
           </Text>
         </Card>
       </Box>
@@ -39,24 +70,44 @@ const Home: React.FC<Props> = ({}) => {
         alignItems="center">
         <Card paddingBottom="l" paddingTop="m" variant="bigPlainCard">
           <ScrollView showsVerticalScrollIndicator={false}>
-            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item, index) => (
+            {data?.map((item, index) => (
               <Item
-                title={'Compra en Walmart'}
-                date="26 de enero, 2019"
-                points={100}
-                isAdded={index % 2 ? true : false}
+                title={item.product}
+                date={showDate(item.createdAt)}
+                points={formatPoints(item.points)}
+                isRedemption={item.is_redemption}
+                image={item.image}
                 key={index}
+                onPress={() => handleNavigateToDetail(index)}
               />
             ))}
           </ScrollView>
         </Card>
       </Box>
       <Box paddingBottom="l" paddingHorizontal="l">
-        <Button
-          variant="buttonPrimary"
-          label="Go to Detail"
-          onPress={() => {}}
-        />
+        {showAll ? (
+          <Button
+            variant="buttonPrimary"
+            label="Todos"
+            onPress={handleShowAll}
+          />
+        ) : (
+          <Box
+            flexDirection="row"
+            alignItems="center"
+            justifyContent="space-between">
+            <Button
+              variant="buttonSecondary"
+              label="Ganados"
+              onPress={handleShowWins}
+            />
+            <Button
+              variant="buttonSecondary"
+              label="Canjeados"
+              onPress={handleShowRedeemed}
+            />
+          </Box>
+        )}
       </Box>
     </Box>
   );
